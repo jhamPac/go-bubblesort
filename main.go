97e0cb96025d8b860400326f86000223bd10dce6
jhamPac <cloud@kevia.me>
2020-03-22 09:23:19 -0700

    basic for loop

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+func sweep(numbers []int) {
+	N := len(numbers)
+
+	firstIndex := 0
+	secondIndex := 1
+
+	for secondIndex < N {
+		n1 := numbers[firstIndex]
+		n2 := numbers[secondIndex]
+
+		fmt.Println("Comparing the following:", n1, n2)
+
+		firstIndex++
+		secondIndex++
+	}
+}
+
 func main() {
 
 }
