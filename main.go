dc63729a545ad4725f5501274a6d8c00f6c1a214
jhamPac <cloud@kevia.me>
2020-03-22 10:22:20 -0700

    early return in sweep is not working

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
-func sweep(numbers []int, prevPasses int) {
+func bubbleSort(numbers []int) {
+	fmt.Printf("Before: %v\n", numbers)
 	N := len(numbers)
 
+	for i := 0; i < N; i++ {
+		if !sweep(numbers, i) {
+			return
+		}
+	}
+
+	fmt.Printf("After: %v", numbers)
+}
+
+func sweep(numbers []int, prevPasses int) bool {
+	N := len(numbers)
 	firstIndex := 0
 	secondIndex := 1
+	didSwap := false
 
 	for secondIndex < (N - prevPasses) {
 		n1 := numbers[firstIndex]
@@ -15,22 +28,13 @@ func sweep(numbers []int, prevPasses int) {
 		if n1 > n2 {
 			numbers[firstIndex] = n2
 			numbers[secondIndex] = n1
+			didSwap = true
 		}
 
 		firstIndex++
 		secondIndex++
 	}
-}
-
-func bubbleSort(numbers []int) {
-	fmt.Printf("Before: %v\n", numbers)
-	N := len(numbers)
-
-	for i := 0; i < N; i++ {
-		sweep(numbers, i)
-	}
-
-	fmt.Printf("After: %v", numbers)
+	return didSwap
 }
 
 func main() {
