7012be048cdc47427a5aead3ab29d8d0900ea8fa
jhamPac <cloud@kevia.me>
2020-03-22 10:38:21 -0700

    nice

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
-func bubbleSort(numbers []int) {
+// Sweeper is a func that can sweep over a slice
+type Sweeper func(numbers []int, prevPasses int) bool
+
+func bubbleSort(numbers []int, fn Sweeper) {
 	N := len(numbers)
 
 	for i := 0; i < N; i++ {
-		if !sweep(numbers, i) {
+		if !fn(numbers, i) {
 			return
 		}
 	}
@@ -34,11 +37,42 @@ func sweep(numbers []int, prevPasses int) bool {
 	return didSwap
 }
 
+func reverseSweep(numbers []int, prevPasses int) bool {
+	N := len(numbers)
+	firstIndex := 0
+	secondIndex := 1
+	didSwap := false
+
+	for secondIndex < (N - prevPasses) {
+		n1 := numbers[firstIndex]
+		n2 := numbers[secondIndex]
+
+		if n1 < n2 {
+			numbers[firstIndex] = n2
+			numbers[secondIndex] = n1
+			didSwap = true
+		}
+
+		firstIndex++
+		secondIndex++
+	}
+	return didSwap
+}
+
 func main() {
 	n := []int{
 		2, 3, 7, 5, 8, 1,
 	}
 	fmt.Printf("Before: %v\n", n)
-	bubbleSort(n)
-	fmt.Printf("After: %v", n)
+	bubbleSort(n, sweep)
+	fmt.Printf("After: %v\n", n)
+
+	fmt.Println("Now sorting in reverse-----------------")
+
+	rn := []int{21, 4, 2, 13, 10, 0, 19, 11, 7, 5, 23, 18, 9, 14, 6, 8, 1, 20, 17, 3, 16, 22, 24, 15, 12}
+
+	fmt.Printf("Before: %v\n", rn)
+	bubbleSort(rn, reverseSweep)
+	fmt.Printf("After: %v\n", rn)
+
 }
